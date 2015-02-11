9088d2b882cabdf5131e2e5c26285f4e6fb13822
Joseph Milla <jjtmilla@gmail.com>
2015-02-11 17:00:16 -0600

    Fix link animation for main page

--- _site/public/js/onload.js
+++ _site/public/js/onload.js
@@ -33,3 +33,14 @@ $(function() {
     event.preventDefault();
   });
 });
+
+$(function() {
+  $('a.more').bind('click',function(event) {
+    var $anchor = $(this);
+
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1250,'easeInOutExpo');
+    event.preventDefault();
+  });
+});
